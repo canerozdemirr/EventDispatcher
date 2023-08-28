ee5c21f36594fcadef9a4c0e93cf0723d6fcf2aa
Caner Özdemir <canerozdemir489@gmail.com>
2023-08-28 23:30:34 +0300

    Project files

---
+++ Assets/EventDispatcher.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDispatcher
+{
+    private static EventDispatcher _instance;
+    public static EventDispatcher Instance => _instance ??= new EventDispatcher();
+    private EventDispatcher() { }
+
+    private Dictionary<Type, List<Delegate>> _eventDictionary = new();
+
+    /// <summary>
+    /// Subscribes a handler to an event of type T.
+    /// </summary>
+    /// <param name="handler">The handler action.</param>
+    /// <typeparam name="T">Event type</typeparam>
+    public void Subscribe<T>(Action<T> handler)
+    {
+        if (handler == null) 
+            return;
+        
+        if (_eventDictionary.TryGetValue(typeof(T), out List<Delegate> existingHandlers))
+        {
+            existingHandlers.Add(handler);
+        }
+        else
+        {
+            _eventDictionary[typeof(T)] = new List<Delegate> { handler };
+        }
+    }
+
+    /// <summary>
+    /// Unsubscribes a handler from an event of type T.
+    /// </summary>
+    /// <param name="handler">The handler action.</param>
+    /// <typeparam name="T">Event type</typeparam>
+    public void Unsubscribe<T>(Action<T> handler)
+    {
+        if (handler == null) 
+            return;
+        
+        if (_eventDictionary.TryGetValue(typeof(T), out List<Delegate> existingHandlers))
+        {
+            existingHandlers.Remove(handler);
+            if (existingHandlers.Count == 0)
+            {
+                _eventDictionary.Remove(typeof(T));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dispatches a handler to with an event of type T.
+    /// </summary>
+    /// <param name="payload">The handler action.</param>
+    /// <typeparam name="T">Event type</typeparam>
+    public void Dispatch<T>(T payload)
+    {
+        if (_eventDictionary.TryGetValue(typeof(T), out List<Delegate> handlers))
+        {
+            if(handlers == null || handlers.Count == 0)
+            {
+                Debug.LogWarning($"No subscribers for event type: {typeof(T)}");
+                return;
+            }
+            foreach (Delegate handler in handlers)
+            {
+                ((Action<T>)handler)?.Invoke(payload);
+            }
+        }
+        
+        else
+        {
+            Debug.LogWarning($"No subscribers for event type: {typeof(T)}");
+        }
+    }
+    
+    /// <summary>
+    /// Unsubscribes all handlers from an event of type T.
+    /// </summary>
+    /// <typeparam name="T">Event type</typeparam>
+    public void UnsubscribeAll<T>()
+    {
+        _eventDictionary.Remove(typeof(T));
+    }
+    
+    /// <summary>
+    /// Clears all event subscriptions.
+    /// </summary>
+    public void Clear()
+    {
+        _eventDictionary.Clear();
+    }
+}
+
